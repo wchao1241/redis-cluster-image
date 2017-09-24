ae30b757305d6f23a82b68c48a17775c9a57e5ab
wangc3 <wangc3@neunn.com>
2017-09-24 16:34:37 +0800

    init

--- run.sh
+++ run.sh
@@ -19,20 +19,19 @@ function launchmaster() {
     echo "Redis master data doesn't exist, data won't be persistent!"
     mkdir /redis-master-data
   fi
-  sed -i "s/%redis-pass%/${REDIS_PASS}/" /redis-master/redis.conf
   redis-server /redis-master/redis.conf --protected-mode no
 }
 
 function launchsentinel() {
   while true; do
-    master=$(redis-cli -a $REDIS_PASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
       master="${master//\"}"
     else
-      master=${REDIS_MASTER_SERVICE_HOST}
+      master=$(hostname -i)
     fi
 
-    redis-cli -a $REDIS_PASS -h ${master} INFO
+    redis-cli -h ${master} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
@@ -43,7 +42,6 @@ function launchsentinel() {
   sentinel_conf=sentinel.conf
 
   echo "sentinel monitor mymaster ${master} 6379 2" > ${sentinel_conf}
-  echo "sentinel auth-pass mymaster ${REDIS_PASS}" >> ${sentinel_conf}
   echo "sentinel down-after-milliseconds mymaster 60000" >> ${sentinel_conf}
   echo "sentinel failover-timeout mymaster 180000" >> ${sentinel_conf}
   echo "sentinel parallel-syncs mymaster 1" >> ${sentinel_conf}
@@ -54,7 +52,7 @@ function launchsentinel() {
 
 function launchslave() {
   while true; do
-    master=$(redis-cli -a $REDIS_PASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
       master="${master//\"}"
     else
@@ -62,7 +60,7 @@ function launchslave() {
       sleep 60
       exit 1
     fi 
-    redis-cli -a $REDIS_PASS -h ${master} INFO
+    redis-cli -h ${master} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
@@ -71,7 +69,6 @@ function launchslave() {
   done
   sed -i "s/%master-ip%/${master}/" /redis-slave/redis.conf
   sed -i "s/%master-port%/6379/" /redis-slave/redis.conf
-  sed -i "s/%redis-pass%/${REDIS_PASS}/" /redis-slave/redis.conf
   redis-server /redis-slave/redis.conf --protected-mode no
 }
 
