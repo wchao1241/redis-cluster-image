4a21c6c40c6ff4589bfbd2c7c0f48b6cbbf9153c
wangc3 <wangc3@neunn.com>
2017-09-29 10:43:44 +0800

    complete

--- run.sh
+++ run.sh
@@ -23,9 +23,6 @@ function launchmaster() {
 }
 
 function launchsentinel() {
-  echo "${HOST}"
-  echo "${PORT}"
-
   while true; do
     master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
